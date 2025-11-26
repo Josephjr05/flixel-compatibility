0b43a5a7ca418faa998bff38065667c6d4e490cf
Joseph <josephjr1506@gmail.com>
2025-11-26 03:07:50 -0500

    ok lemme stop lol lets install this

--- flixel/FlxCamera.hx
+++ flixel/FlxCamera.hx
@@ -2165,7 +2165,6 @@ class FlxCamera extends FlxBasic
 	{
 		return filters = Value;
 	}
-	// test githu
 	
 	/**
 	 * Do not use the following fields! They only exists because FlxCamera extends FlxBasic,
