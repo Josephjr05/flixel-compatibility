3998ecfdfe12732afb38061e66a849f3e72b6035
Joseph <josephjr1506@gmail.com>
2026-06-23 23:41:56 -0400

    Update to latest flixel fully

--- flixel/text/FlxBitmapText.hx
+++ flixel/text/FlxBitmapText.hx
@@ -375,7 +375,7 @@ class FlxBitmapText extends FlxSprite
 					continue;
 				}
 
-				getScreenPosition(screenPos, camera).subtractPoint(offset);
+				getScreenPosition(screenPos, camera).subtract(offset);
 
 				if (isPixelPerfectRender(camera))
 				{
@@ -1038,7 +1038,7 @@ class FlxBitmapText extends FlxSprite
 		for (i in 0...numLines)
 		{
 			line = _lines[i];
-			lineWidth = _linesWidth[i];
+			lineWidth = getLineWidth(i);
 
 			// LEFT
 			ox = font.minOffsetX;
